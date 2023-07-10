cb41c5e862874cb4da086a6e45f4ad8357b6f122
Ricardo Alves <dev.rical21@gmail.com>
2023-07-10 18:23:24 -0300

    Incluindo banco de dados mysql

--- crud-spring/src/main/java/com/ricardo/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/ricardo/CrudSpringApplication.java
@@ -1,12 +1,7 @@
 package com.ricardo;
 
-import com.ricardo.enums.Etapa;
-import com.ricardo.model.Tarefa;
-import com.ricardo.repository.TarefaRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -14,23 +9,5 @@ public class CrudSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
-
-	//A anotação 'Bean' diz pro spring gerenciar o ciclo de vida deste fluxo
-	//Este comando 'CommandLineRunner' coloca este comando pra ser executado assim que esta aplicação subir
-//	@Bean
-//	CommandLineRunner initDataBase(TarefaRepository tarefaRepository) {
-//
-//		return args -> {
-//			tarefaRepository.deleteAll();
-//
-//			Tarefa t = new Tarefa();
-//			t.setConteudo("Criar portfólio Java");
-//			t.setResponsavel("Ricardo Alves");
-//			t.setEtapa(Etapa.AFAZER);
-//
-//			tarefaRepository.save(t);
-//		};
-//
-//	}
 
 }
